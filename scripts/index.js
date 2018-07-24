35dd952cec1ad4c79433863a09e06503e22e0056
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-07-24 14:36:14 -0400

    decorateResponse function working

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,184 @@
 'use strict';
 /* global $ */
-const API_KEY = 'YOUR_KEY_HERE';
+const API_KEY = 'AIzaSyCkyNYYCLfSOR83jC4PN6TAAzQNwmun6Ko';
+
+const mockData = {
+  "kind": "youtube#searchListResponse",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/nWpornfimyBcKMYH0-S18JHOzlM\"",
+  "nextPageToken": "CAUQAA",
+  "regionCode": "US",
+  "pageInfo": {
+  "totalResults": 1000000,
+  "resultsPerPage": 5
+  },
+  "items": [
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/Xv0f71hRivXbOFeZ14SKHWY1IJs\"",
+  "id": {
+  "kind": "youtube#video",
+  "videoId": "EtH9Yllzjcc"
+  },
+  "snippet": {
+  "publishedAt": "2017-11-18T21:00:00.000Z",
+  "channelId": "UC3rrzHpFzshYjIMk8YFc52w",
+  "title": "Try Not To Laugh Challenge - Funny Cat & Dog Vines compilation 2017",
+  "description": "Try Not To Laugh or Grin While Watching Funny Dogs & Cats Vines compilation 2017 IMPOSSIBLE CHALLENGE! Subscribe to CooL Vines ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://i.ytimg.com/vi/EtH9Yllzjcc/default.jpg",
+  "width": 120,
+  "height": 90
+  },
+  "medium": {
+  "url": "https://i.ytimg.com/vi/EtH9Yllzjcc/mqdefault.jpg",
+  "width": 320,
+  "height": 180
+  },
+  "high": {
+  "url": "https://i.ytimg.com/vi/EtH9Yllzjcc/hqdefault.jpg",
+  "width": 480,
+  "height": 360
+  }
+  },
+  "channelTitle": "CooL Vines",
+  "liveBroadcastContent": "none"
+  }
+  },
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/CIzHxn5Tg3A4s2nJ1YNkNe9bwj8\"",
+  "id": {
+  "kind": "youtube#video",
+  "videoId": "hY7m5jjJ9mM"
+  },
+  "snippet": {
+  "publishedAt": "2017-05-31T09:30:02.000Z",
+  "channelId": "UC9obdDRxQkmn_4YpcBMTYLw",
+  "title": "CATS will make you LAUGH YOUR HEAD OFF - Funny CAT compilation",
+  "description": "Cats are amazing creatures because they make us laugh all the time! Watching funny cats is the hardest try not to laugh challenge! Just look how all these cats ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://i.ytimg.com/vi/hY7m5jjJ9mM/default.jpg",
+  "width": 120,
+  "height": 90
+  },
+  "medium": {
+  "url": "https://i.ytimg.com/vi/hY7m5jjJ9mM/mqdefault.jpg",
+  "width": 320,
+  "height": 180
+  },
+  "high": {
+  "url": "https://i.ytimg.com/vi/hY7m5jjJ9mM/hqdefault.jpg",
+  "width": 480,
+  "height": 360
+  }
+  },
+  "channelTitle": "Tiger FunnyWorks",
+  "liveBroadcastContent": "none"
+  }
+  },
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/rXb4BnutEQ543H8-7A55h0jqUvI\"",
+  "id": {
+  "kind": "youtube#video",
+  "videoId": "tzI2g2Uiz8w"
+  },
+  "snippet": {
+  "publishedAt": "2018-07-06T12:00:05.000Z",
+  "channelId": "UCyIqcxz-vR_o2GK4HWuZL8w",
+  "title": "Cat's maze. Where's the exit???",
+  "description": "The entrance is blocked. The walls are one meter high. No way to find a loophole or a trapdoor. The only one way to escape is to find the exit. Will the cat be able ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://i.ytimg.com/vi/tzI2g2Uiz8w/default.jpg",
+  "width": 120,
+  "height": 90
+  },
+  "medium": {
+  "url": "https://i.ytimg.com/vi/tzI2g2Uiz8w/mqdefault.jpg",
+  "width": 320,
+  "height": 180
+  },
+  "high": {
+  "url": "https://i.ytimg.com/vi/tzI2g2Uiz8w/hqdefault.jpg",
+  "width": 480,
+  "height": 360
+  }
+  },
+  "channelTitle": "CatPusic",
+  "liveBroadcastContent": "none"
+  }
+  },
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/x_qS9OFGWgrqGJL3crMvWY_oYxs\"",
+  "id": {
+  "kind": "youtube#video",
+  "videoId": "RIBsfhcAZww"
+  },
+  "snippet": {
+  "publishedAt": "2018-06-15T07:43:26.000Z",
+  "channelId": "UCNo5PGwGmfnEprEUUb9AGgw",
+  "title": "World Cup Cats",
+  "description": "The World Cup inspires Prince Michael's journey towards soccer stardom. New Merch: https://shop.aaronsanimals.com/ Gym Girl: Shelise Hufstetler Ref: Robert ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://i.ytimg.com/vi/RIBsfhcAZww/default.jpg",
+  "width": 120,
+  "height": 90
+  },
+  "medium": {
+  "url": "https://i.ytimg.com/vi/RIBsfhcAZww/mqdefault.jpg",
+  "width": 320,
+  "height": 180
+  },
+  "high": {
+  "url": "https://i.ytimg.com/vi/RIBsfhcAZww/hqdefault.jpg",
+  "width": 480,
+  "height": 360
+  }
+  },
+  "channelTitle": "AaronsAnimals",
+  "liveBroadcastContent": "none"
+  }
+  },
+  {
+  "kind": "youtube#searchResult",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/CYY2TYr5zHclu-VtAEK3vReRew4\"",
+  "id": {
+  "kind": "youtube#video",
+  "videoId": "SyPTm7IwFuc"
+  },
+  "snippet": {
+  "publishedAt": "2018-06-19T02:07:29.000Z",
+  "channelId": "UCYFo16eFDNhD3W_POj9gKFw",
+  "title": "Talking Kitty Cat - Meet The New Kitten!",
+  "description": "Sylvester, Random kitty and Gibbyson get to meet the latest addition to the family. Random kitty learns how to be a grandmother, Gibbyson isn't sure how to be ...",
+  "thumbnails": {
+  "default": {
+  "url": "https://i.ytimg.com/vi/SyPTm7IwFuc/default.jpg",
+  "width": 120,
+  "height": 90
+  },
+  "medium": {
+  "url": "https://i.ytimg.com/vi/SyPTm7IwFuc/mqdefault.jpg",
+  "width": 320,
+  "height": 180
+  },
+  "high": {
+  "url": "https://i.ytimg.com/vi/SyPTm7IwFuc/hqdefault.jpg",
+  "width": 480,
+  "height": 360
+  }
+  },
+  "channelTitle": "Talking Kitty Cat",
+  "liveBroadcastContent": "none"
+  }
+  }
+  ]
+};
 
 /*
   We want our store to hold a `videos` array of "decorated" objects - i.e. objects that
@@ -34,14 +212,10 @@ const fetchVideos = function(searchTerm, callback) {
     q: searchTerm,
     type: '',
   };
-  console.log(query);
-  $.getJSON(BASE_URL, query, function(response) {
-    console.log(response);
-  });      
-      
+  $.getJSON(BASE_URL, query, callback);
 };
-fetchVideos('cats');
 
+// fetchVideos('cat', (response) => console.log(response));
 // TASK:
 // 1. Create a `decorateResponse` function that receives the Youtube API response
 // 2. Map through the response object's `items` array
@@ -51,7 +225,13 @@ fetchVideos('cats');
 // TEST IT! Grab an example API response and send it into the function - make sure
 // you get back the object you want.
 const decorateResponse = function(response) {
-
+  return response.items.map(item => {
+    return {
+      id: item.id.videoId,
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.default.url
+    };
+  });
 };
 
 // TASK:
@@ -59,7 +239,8 @@ const decorateResponse = function(response) {
 // 2. Using the object, return an HTML string containing all the expected data
 // TEST IT!
 const generateVideoItemHtml = function(video) {
-
+  return '
+  '
 };
 
 // TASK:
